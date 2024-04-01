f8325031f7a524890b0c31a988eef0e5c75007db
Kamel R <kamel.r@wayne.edu>
2024-04-01 18:29:58 -0400

    added the insertatstart function

--- adtlinkedlist.cpp
+++ adtlinkedlist.cpp
@@ -17,3 +17,18 @@ int adtlinkedlist::getLength()
 {
 	return count;
 }
+
+void adtlinkedlist::insertAtStart(int data)
+{
+	// 1. create a node 
+	node* newNode = new node();
+	newNode->info = data;
+
+	// 2. have the link for the new node pointing to what the first pointer is pointing to 
+	newNode->link = first;
+	// 3. have the first pointer pointing to the new node
+	first = newNode;
+	// 4. increment the count 
+	count++;
+
+}
